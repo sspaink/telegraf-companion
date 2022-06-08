35b1a775621176cdc7fc1c36ae09a77b46594ce5
Sebastian Spaink <sspaink@influxdata.com>
2022-06-08 17:06:18 -0500

    Add release target to magefile

--- go.mod
+++ go.mod
@@ -6,43 +6,52 @@ require (
 	github.com/charmbracelet/bubbles v0.10.2
 	github.com/charmbracelet/bubbletea v0.19.3
 	github.com/charmbracelet/lipgloss v0.4.0
-	github.com/dave/jennifer v1.5.0
 	github.com/go-git/go-git/v5 v5.4.2
 	github.com/magefile/mage v1.13.0
 	github.com/mattn/go-runewidth v0.0.13
+	github.com/mholt/archiver/v4 v4.0.0-alpha.7
 	github.com/muesli/reflow v0.3.0
-	github.com/yuin/goldmark v1.4.1
 )
 
 require (
 	github.com/Microsoft/go-winio v0.4.17 // indirect
 	github.com/ProtonMail/go-crypto v0.0.0-20210428141323-04723f9f07d7 // indirect
 	github.com/acomagu/bufpipe v1.0.3 // indirect
+	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/containerd/console v1.0.2 // indirect
+	github.com/dsnet/compress v0.0.1 // indirect
 	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/go-git/gcfg v1.5.0 // indirect
 	github.com/go-git/go-billy/v5 v5.3.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/kevinburke/ssh_config v0.0.0-20201106050909-4977a11b4351 // indirect
+	github.com/klauspost/compress v1.15.5 // indirect
+	github.com/klauspost/pgzip v1.2.5 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
 	github.com/muesli/termenv v0.9.0 // indirect
+	github.com/nwaples/rardecode/v2 v2.0.0-beta.2 // indirect
+	github.com/pierrec/lz4/v4 v4.1.14 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.6.2 // indirect
 	github.com/sahilm/fuzzy v0.1.0 // indirect
 	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/therootcompany/xz v1.0.1 // indirect
+	github.com/ulikunitz/xz v0.5.10 // indirect
 	github.com/xanzy/ssh-agent v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
 	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
 	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
+	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
--- magefile.go
+++ magefile.go
@@ -3,42 +3,166 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
+	"github.com/mholt/archiver/v4"
 )
 
-const binName = "telegraf-companion"
+const releaseVersion = "v0.1.0"
+
+var productName = "telegraf-companion"
+
+type platform struct {
+	OS            string
+	ARCH          string
+	Extension     string
+	ArchiveFormat string
+}
+
+var platforms = []platform{
+	{
+		OS:            "linux",
+		ARCH:          "amd64",
+		ArchiveFormat: "tar.gz",
+	},
+	{
+		OS:            "windows",
+		ARCH:          "amd64",
+		Extension:     ".exe",
+		ArchiveFormat: "zip",
+	},
+	{
+		OS:            "darwin",
+		ARCH:          "amd64",
+		ArchiveFormat: "tar.gz",
+	},
+}
+
+// Release packages all the binaries and records checksums
+func Release() error {
+	if _, err := os.Stat("release"); os.IsNotExist(err) {
+		os.MkdirAll("release", 0777) // Create your file
+	}
+
+	var binPaths []string
+	for _, p := range platforms {
+		binPath, err := build(p)
+		if err != nil {
+			return err
+		}
+		binPaths = append(binPaths, binPath)
+
+		// map files on disk to their paths in the archive
+		files, err := archiver.FilesFromDisk(nil, map[string]string{
+			binPath:     filepath.Base(binPath),
+			"LICENSE":   "",
+			"README.md": "",
+		})
+		if err != nil {
+			return err
+		}
+
+		// create the output file we'll write to
+		fileName := fmt.Sprintf("%s_%s_%s_%s.%s", productName, releaseVersion, p.OS, p.ARCH, p.ArchiveFormat)
+		out, err := os.Create(filepath.Join("release", fileName))
+		if err != nil {
+			return err
+		}
+		defer out.Close()
+
+		// we can use the CompressedArchive type to gzip a tarball
+		// (compression is not required; you could use Tar directly)
+		var format archiver.CompressedArchive
+		switch p.ArchiveFormat {
+		case "tar.gz":
+			format = archiver.CompressedArchive{
+				Compression: archiver.Gz{},
+				Archival:    archiver.Tar{},
+			}
+		case "zip":
+			format = archiver.CompressedArchive{
+				Archival: archiver.Zip{},
+			}
+		}
+
+		// create the archive
+		err = format.Archive(context.Background(), out, files)
+		if err != nil {
+			return err
+		}
+	}
+
+	checksumFilename := fmt.Sprintf("%s_%s_checksum.txt", productName, releaseVersion)
+	checksumPath := filepath.Join("release", checksumFilename)
+	f, err := os.Create(checksumPath)
+	if err != nil {
+		return err
+	}
+
+	w := bufio.NewWriter(f)
+
+	for _, f := range binPaths {
+		output, err := sh.OutputWith(map[string]string{}, "sha256sum", f)
+		if err != nil {
+			fmt.Fprint(os.Stderr, output)
+		}
+		w.WriteString(output + "\n")
+	}
+
+	w.Flush()
+
+	return nil
+}
 
 type Build mg.Namespace
 
 // Builds a Linux binary 64 bit.
 func (Build) Linux() error {
-	return build("linux", "amd64", "")
+	_, err := build(platforms[0])
+	return err
 }
 
 // Builds a Windows binary 64 bit.
 func (Build) Windows() error {
-	return build("windows", "amd64", ".exe")
+	_, err := build(platforms[1])
+	return err
 }
 
 // Builds a Darwin binary 64 bit.
 func (Build) Darwin() error {
-	return build("darwin", "amd64", "")
+	_, err := build(platforms[2])
+	return err
 }
 
-func build(system string, platform string, extension string) error {
-	env := map[string]string{"GOOS": system, "GOARCH": platform}
-	return runCmd(env, "go", "build", "-o", binName+extension, "cmd/main.go")
+// build will build telegraf-companion for the passed platform and returns the filepath
+func build(p platform) (string, error) {
+	env := map[string]string{"GOOS": p.OS, "GOARCH": p.ARCH}
+	folderName := fmt.Sprintf("%s_%s", p.OS, p.ARCH)
+	binName := productName + p.Extension
+	filePath := filepath.Join("bin", folderName, binName)
+	err := runCmd(env, "go", "build", "-o", filePath, "cmd/main.go")
+	if err != nil {
+		return "", err
+	}
+
+	return filePath, nil
+
 }
 
+// Test runs all go tests.
 func Test() error {
 	return runCmd(map[string]string{}, "go", "test", "./...")
 }
 
-func UpdateSampleconfigs() error {
+// UpdateSampleConfigs updates the plugin sample configurations to the latest version
+func UpdateSampleConfigs() error {
+	// TODO: this should update the telegraf version mentioned in README.md
 	return runCmd(map[string]string{}, "go", "generate", "./plugins/sampleConfigs.go")
 }
 
